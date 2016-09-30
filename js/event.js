d06b3aaebd10f76be7965bc49ceedb4cbc5cfc7b
paulweaver2011 <paulweaver2011@gmail.com>
2016-09-30 11:49:26 -0600

    Today is the day!

--- js/event.js
+++ js/event.js
@@ -1,14 +1,28 @@
-var button = document.getElementById("button1");
-var startButton = document.getElementById("startButton")
-
-var myFunction = function(myFunction){
-    alert("Hello world!")
+var userName;
+var button = document.getElementById("name");
+var enterName = function myFunctionHandler(){
+    userName = prompt("Enter your name.")
 }
 
-button1.addEventListener ("click", myFunction)
+button.addEventListener ("click", enterName);
 
-var startButton = document.getElementById("startButton")
+var startButton = document.getElementById("startButton");
 
-var startGameHandler = function startMyGame(startButton){
-    prompt
+var startGameHandler = function startMyGame(){
+    if(userName == undefined){
+        alert("Please enter a valid name");
+    } else {
+        alert("Ready player one!");
+    }
 }
+
+var gameButton = function GameButtonFondler(name){
+    var button;
+    var fire = function(){
+        alert("LASER BEEEEEEAAAAAMMMMMMMMMM");
+    }
+}
+
+var fireWeaponButton = new gameButton("fireWeapon");
+fireWeaponButton.button = document.getElementById("fireWeapon");
+fireWeaponButton.button.addEventListener("click", fireWeaponButton.fire);
